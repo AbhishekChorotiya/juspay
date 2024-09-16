5e17ec7e96d69e9a287b2099939fd663fc5b4536
abhishek <abhishekchorotiya@openinapp.com>
2024-09-17 01:23:43 +0530

    complete  basic ui

--- src/App.js
+++ src/App.js
@@ -9,14 +9,13 @@ export default function App() {
   const [data, setData] = useState({});
   return (
     <GlobalContext.Provider value={{ data, setData }}>
-      <div className="bg-blue-100 flex h-screen flex-col w-screen p-2 font-sans">
-        <div className="mb-2">{"Scratch starter project"}</div>
-        <div className="w-full h-full overflow-hidden flex">
-          <div className="flex-1 h-screen overflow-hidden flex flex-row bg-white border-t border-r border-gray-200 rounded-tr-xl mr-2">
-            {/* <Sidebar /> <MidArea /> */}
+      <div className="bg-blue-100 flex h-dvh flex-col w-screen p-2 font-sans">
+        {/* <div className="mb-2">{"Scratch starter project"}</div> */}
+        <div className="w-full h-full flex">
+          <div className="flex-1 h-full overflow-hidden  flex flex-row bg-white border-t border-r border-gray-200 rounded-xl mr-2">
             <DnD />
           </div>
-          <div className="w-1/3 h-screen overflow-hidden flex flex-row bg-white border-t border-l border-gray-200 rounded-tl-xl ml-2">
+          <div className="w-2/5 h-full  flex flex-row bg-white border-t border-l border-gray-200 rounded-xl overflow-hidden">
             <PreviewArea />
           </div>
         </div>
--- src/components/DnD.js
+++ src/components/DnD.js
@@ -1,8 +1,12 @@
-import { useContext, useCallback, useMemo } from "react";
+import { useContext, useCallback, useMemo, useRef, useState } from "react";
 import { BlocklyWorkspace } from "react-blockly";
 import * as Blockly from "blockly";
 import { controlBlocks, eventBlocks, motionBlocks } from "../utils/constants";
 import { GlobalContext } from "../App";
+import Dropdown from "./Dropdown";
+import { useAtom } from "jotai";
+import { spritesAtom } from "../utils/atoms";
+import CatSprite from "./CatSprite";
 
 Blockly.defineBlocksWithJsonArray([
   ...eventBlocks,
@@ -43,22 +47,70 @@ function DnD() {
   const { setData } = useContext(GlobalContext);
   const toolboxConfig = useToolboxConfig();
   const workspaceConfig = useWorkspaceConfig();
+  const savedWorkspace = useRef({ 1: null, 2: null });
+  const [selectedSprite, setSelectedSprite] = useState("Cat");
+  const index = useRef(1);
+  const [sprites, setSprites] = useAtom(spritesAtom);
+
+  const options = [
+    { name: "Cat", src: <CatSprite /> },
+    { name: "Ball", src: <CatSprite /> },
+  ];
 
   const handleJsonChange = useCallback(
     (e) => {
-      console.log(e.blocks.blocks);
+      console.log(e?.blocks?.blocks);
       setData(e?.blocks?.blocks);
     },
     [setData]
   );
 
+  function handleSave(id) {
+    const currentWorkspace = Blockly.getMainWorkspace();
+    savedWorkspace.current[id] =
+      Blockly.serialization.workspaces.save(currentWorkspace);
+    currentWorkspace.clear();
+  }
+  function handleOpen(id) {
+    const workspace = Blockly.getMainWorkspace();
+    if (workspace) {
+      workspace.clear(); // Clear workspace before loading new state
+      Blockly.serialization.workspaces.load(
+        savedWorkspace.current[id],
+        workspace
+      );
+    }
+  }
+
+  const handleSelectedSprite = (option) => {
+    setSelectedSprite(option?.name);
+    const sprite = {
+      id: ++index.current,
+      name: option?.name,
+      src: option?.src,
+      top: `${Math.random() * 500 + 50}px`,
+      left: `${Math.random() * 500 + 50}px`,
+    };
+    setSprites([...sprites, sprite]);
+  };
+
   return (
-    <BlocklyWorkspace
-      onJsonChange={handleJsonChange}
-      className="w-full h-full"
-      toolboxConfiguration={toolboxConfig}
-      workspaceConfiguration={workspaceConfig}
-    />
+    <div className="flex relative flex-col min-w-full h-full">
+      <BlocklyWorkspace
+        onJsonChange={handleJsonChange}
+        className="w-full h-full"
+        toolboxConfiguration={toolboxConfig}
+        workspaceConfiguration={workspaceConfig}
+      />
+      <div className="absolute z-[10000] flex items-center gap-2 top-0 bg-yellow-300/40 right-0 rounded-bl-xl  px-4 py-2 text-black">
+        <span>Add : </span>
+        <Dropdown
+          selected={selectedSprite}
+          setSelected={handleSelectedSprite}
+          options={options}
+        />
+      </div>
+    </div>
   );
 }
 
